3e68ec64f6f17a0185cad323bca43f2d6ff8a0ee
William <william.call@live.com>
2020-03-15 19:39:02 -0700

    halfway done.

--- src/domain/help/templates/advancedBoards.tpl.php
+++ src/domain/help/templates/advancedBoards.tpl.php
@@ -9,9 +9,8 @@
             <div style='width:50%' class='svgContainer'>
                 <?php    echo file_get_contents(ROOT."/images/svg/undraw_new_ideas_jdea.svg");
                 echo"</div>";?>
-            <h3 class="primaryColor">Welcome to the organized idea board!</h3><br />
-            <p>Ideas evolve, grow and mature. This is the place to keep track of them. <br />Move idea cards from one column to the next to reflect their current state.<br />
-            This is a good place to start research, develop prototypes and validate these prototypes with a few customers before your start implementing.</p>
+            <h3 class="primaryColor"><?php echo $this->__('headlines.welcome_to_organized_idea_board') ?></h3><br />
+            <p><?php echo $this->__('text.ideas_evolve') ?>
             <br /><br />
         </div>
     </div>
@@ -21,8 +20,8 @@
         <div class="col-md-12">
             <p>
              </p>
-            <a href="javascript:void(0);"  onclick="jQuery.nmTop().close()"><i class="fa fa-close"></i> Close</a><br />
-            <a href="javascript:void(0);" onclick="leantime.helperController.hideAndKeepHidden('advancedIdeaBoards')">Close and don't show this screen again</a>
+            <a href="javascript:void(0);"  onclick="jQuery.nmTop().close()"><?php echo $this->__('links.close') ?></a><br />
+            <a href="javascript:void(0);" onclick="leantime.helperController.hideAndKeepHidden('advancedIdeaBoards')"><?php echo $this->__('this.close_dont_show_again') ?></a>
         </div>
     </div>
 
--- src/domain/help/templates/fullLeanCanvas.tpl.php
+++ src/domain/help/templates/fullLeanCanvas.tpl.php
@@ -9,19 +9,8 @@
             <div style='width:50%' class='svgContainer'>
                 <?php    echo file_get_contents(ROOT."/images/svg/undraw_design_data_khdb.svg");
                 echo"</div>";?>
-            <h3 class="primaryColor">Welcome to the Full Research Board!</h3><br />
-            <p>The full research board will take you through the full idea validation process.  <br/>
-            On this page, you'll again address your Customer Segment, Problem, Solution (but don't worry, if you filled that out on the Simple board, you'll see it again here!).<br/>
-                <br/>From there, you'll move into <br/>
-                Unique Value Proposition (What makes your idea better than other offerings?), <br/>
-                Distribution Channels (How will you get your solution to your customers?)<br/>
-                Revenue Streams (How will your solution make money?)<br/>
-                Cost Structure (How much is your solution worth?)<br/>
-                Key Metrics (How will you measure success?)<br/>
-                and Unfair Advantage (What do you have that will help you succeed against the competition?)<br/>
-
-                Answering these questions are designed to promote success, mitigate failure, and allow you to track and validate your assumptions.  Being Lean is about eliminating waste </br>
-                and working to build products, business, and solutions that your customers need.(</p>
+            <h3 class="primaryColor"><?php echo $this->__('headlines.welcome_to_research_board') ?></h3><br />
+            <p><?php echo $this->__('text.the_full_research_board') ?></p>
             <br /><br />
         </div>
     </div>
@@ -31,8 +20,8 @@
         <div class="col-md-12">
             <p>
              </p>
-            <a href="javascript:void(0);"  onclick="jQuery.nmTop().close()"><i class="fa fa-close"></i> Close</a><br />
-            <a href="javascript:void(0);" onclick="leantime.helperController.hideAndKeepHidden('fullLeanCanvas')">Close and don't show this screen again</a>
+            <a href="javascript:void(0);"  onclick="jQuery.nmTop().close()"><?php echo $this->__('links.close') ?></a><br />
+            <a href="javascript:void(0);" onclick="leantime.helperController.hideAndKeepHidden('fullLeanCanvas')"><?php echo $this->__('links.close_dont_show_again') ?></a>
         </div>
     </div>
 
