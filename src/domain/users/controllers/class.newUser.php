f74cb3de96c371b5dbfb62a01b665d00c0df4e31
Marcel Folaron <marcel@leantime.io>
2022-04-16 11:48:32 -0400

    inew user clean up

--- src/domain/users/controllers/class.newUser.php
+++ src/domain/users/controllers/class.newUser.php
@@ -55,10 +55,6 @@ namespace leantime\domain\controllers {
 						$values['clientId'] = core\login::getUserClientId();
 					}
 
-					//Validation
-					//print_r($_POST);
-					# exit();
-
 					if ($values['user'] !== '') {
 						if ($_POST['password'] == $_POST['password2']) {
 							if (filter_var($values['user'], FILTER_VALIDATE_EMAIL)) {
@@ -66,7 +62,6 @@ namespace leantime\domain\controllers {
 									if ($userRepo->usernameExist($values['user']) === false) {
 
 										$userId = $userRepo->addUser($values);
-
 
 										//Update Project Relationships
 										if (isset($_POST['projects'])) {
