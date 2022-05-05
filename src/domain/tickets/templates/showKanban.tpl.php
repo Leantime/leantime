1f8f80547788ef6976f78e12058385d76e4a7872
Axel Rindle <mail@axelrindle.de>
2022-05-05 10:24:32 +0200

    Display ticket ID

--- src/domain/tickets/templates/showKanban.tpl.php
+++ src/domain/tickets/templates/showKanban.tpl.php
@@ -267,6 +267,7 @@
                                                     </div>
                                                 <?php } ?>
                                                 <small><i class="fa <?php echo $todoTypeIcons[strtolower($row['type'])]; ?>"></i> <?php echo $this->__("label.".strtolower($row['type'])); ?></small>
+                                                <small>#<?php echo $row['id']; ?></small>
 
                                                 <h4><a href="<?=BASE_URL ?>/tickets/showTicket/<?php echo $row["id"];?>"><?php $this->e($row["headline"]);?></a></h4>
                                                 <p class="description"><?php echo $this->truncate(html_entity_decode($row["description"]), 200, '...', false, true);?><?php if(strlen($row["description"]) >= 200) echo" (...)";?></p>
