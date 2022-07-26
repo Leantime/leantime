85f5bb3de64513097b71fa6bb3211e7a77fc9793
Marcel Folaron <marcel.folaron@gmail.com>
2022-07-25 20:47:27 -0400

    Clean up font files

--- public/js/libs/jquery.nyroModal/js/jquery.nyroModal.custom.js
+++ public/js/libs/jquery.nyroModal/js/jquery.nyroModal.custom.js
@@ -656,21 +656,12 @@ jQuery(function($, undefined) {
 
 			_scrollWidth: (function() {
 				var scrollbarWidth;
-				if ($.browser.msie) {
-					var $textarea1 = $('<textarea cols="10" rows="2"></textarea>')
-							.css({ position: 'absolute', top: -1000, left: -1000 }).appendTo($b),
-						$textarea2 = $('<textarea cols="10" rows="2" style="overflow: hidden;"></textarea>')
-							.css({ position: 'absolute', top: -1000, left: -1000 }).appendTo($b);
-					scrollbarWidth = $textarea1.width() - $textarea2.width();
-					$textarea1.add($textarea2).remove();
-				} else {
-					var $div = $('<div />')
-						.css({ width: 100, height: 100, overflow: 'auto', position: 'absolute', top: -1000, left: -1000 })
-						.prependTo($b).append('<div />').find('div')
-							.css({ width: '100%', height: 200 });
-					scrollbarWidth = 100 - $div.width();
-					$div.parent().remove();
-				}
+				var $div = $('<div />')
+					.css({ width: 100, height: 100, overflow: 'auto', position: 'absolute', top: -1000, left: -1000 })
+					.prependTo($b).append('<div />').find('div')
+					.css({ width: '100%', height: 200 });
+				scrollbarWidth = 100 - $div.width();
+				$div.parent().remove();
 				return scrollbarWidth;
 			})(),
 
