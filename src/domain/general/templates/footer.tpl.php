fe3781378d72150620218b588b9f46b318a3eb23
pvi-github <pvi-github@vilarem.ne>
2022-07-30 10:25:51 +0200

    [FIX] Fixing typo in cron.php call URL (footer template)

--- src/domain/general/templates/footer.tpl.php
+++ src/domain/general/templates/footer.tpl.php
@@ -3,6 +3,6 @@
 <p><a href="http://leantime.io" target="_blank">Powered By Leantime</a> - <?=$this->get("version");?></p>
 <script>
     var req = new XMLHttpRequest();
-    req.open("GET", "<?=BASE_URL?>c/cron.php",true);
+    req.open("GET", "<?=BASE_URL?>/cron.php",true);
     req.send(null);
 </script>
