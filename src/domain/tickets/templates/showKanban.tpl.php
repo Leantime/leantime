91764e18616224714d7ffb214a9a55bc8f1c7c63
Claude Diderich <diderich@yahoo.com>
2022-10-22 13:06:10 +0200

    My suggestion for fixing issue #1070

--- src/domain/tickets/templates/showKanban.tpl.php
+++ src/domain/tickets/templates/showKanban.tpl.php
@@ -288,7 +288,7 @@
                                                 <?php } ?>
                                                 <small><i class="fa <?php echo $todoTypeIcons[strtolower($row['type'])]; ?>"></i> <?php echo $this->__("label.".strtolower($row['type'])); ?></small>
                                                 <small>#<?php echo $row['id']; ?></small>
-                                                <div class="kanbanCardContent" style="margin-bottom:20px;">
+                                                <div class="kanbanCardContent">
 
                                                     <h4><a class='ticketModal' href="<?=BASE_URL ?>/tickets/showTicket/<?php echo $row["id"];?>"><?php $this->e($row["headline"]);?></a></h4>
 
