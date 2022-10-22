6b4b9a49ff45163f160c514a5ab600124dcc301c
Claude Diderich <diderich@yahoo.com>
2022-10-22 12:01:10 +0200

    Code layout improvements

--- src/domain/canvas/templates/delCanvas.inc.php
+++ src/domain/canvas/templates/delCanvas.inc.php
@@ -26,7 +26,7 @@ $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
                 <p><?php echo $this->__('text.confirm_board_deletion'); ?></p><br />
                 <input type="submit" value="<?php echo $this->__('buttons.yes_delete'); ?>" name="del" class="button" />
                 <a class="btn btn-secondary"
-				   href="<?=BASE_URL ?>/<?=$canvasName ?>canvas/showCanvas"><?php echo $this->__('buttons.back'); ?></a>
+                   href="<?=BASE_URL ?>/<?=$canvasName ?>canvas/showCanvas"><?php echo $this->__('buttons.back'); ?></a>
             </form>
         </div>
 
