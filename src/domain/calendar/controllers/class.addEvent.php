a6845b14c52e940a57bdf169e0e425186a527b85
Marcel Folaron <marcel.folaron@gmail.com>
2022-11-10 08:40:31 -0500

    remove var_dump()

--- src/domain/calendar/controllers/class.addEvent.php
+++ src/domain/calendar/controllers/class.addEvent.php
@@ -66,8 +66,6 @@ namespace leantime\domain\controllers {
                     'allDay' => $allDay
                 );
 
-                var_dump($values);
-
                 if ($values['description'] !== '') {
 
                     $calendarRepo->addEvent($values);
