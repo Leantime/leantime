9622610978b795e257ea6f4e4d41387f9f4b7d9b
Marcel Folaron <marcel.folaron@gmail.com>
2022-12-30 06:54:36 -0500

    Fix missing s in copy URL

--- src/domain/menu/repositories/class.menu.php
+++ src/domain/menu/repositories/class.menu.php
@@ -70,7 +70,7 @@ namespace leantime\domain\repositories {
             $language = core\language::getInstance();
             $config = \leantime\core\environment::getInstance();
 
-            if (!$config->enableMenuType) {
+            if (isset($config->enableMenuType) && !$config->enableMenuType) {
 
                 return [self::DEFAULT_MENU => $language->__('label.menu_type.' . self::DEFAULT_MENU)];
             }
