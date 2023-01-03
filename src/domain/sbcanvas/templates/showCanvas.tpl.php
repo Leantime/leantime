ef783a699220242fdae7061a7305466eecc864cf
Marcel Folaron <marcel.folaron@gmail.com>
2023-01-02 23:33:16 -0500

    Updated project path to blueprints

--- src/domain/sbcanvas/templates/showCanvas.tpl.php
+++ src/domain/sbcanvas/templates/showCanvas.tpl.php
@@ -17,12 +17,11 @@ $canvasName = 'sb';
         <div id="sortableCanvasKanban" class="sortableTicketList disabled">
           <div class="row-fluid"><div class="column" style="width: 100%; min-width: calc(4 * 250px);">
 
-            <div class="row canvas-row">
-                <div class="column" style="width:100%">
-		          <h4 class="widgettitle title-primary center"><i class='fas fa-list-check'></i> <?=$this->__('box.sb.title') ?></h4>
-                  <div class="contentInner even" style="padding-top: 10px;"><?=$this->e($_SESSION['currentProjectName']); ?></div>
-                </div>
-            </div>
+              <div class="row canvas-row">
+                  <div class="column" style="width:100%">
+                      <?php $elementName = 'sb_description'; require($this->getTemplatePath('canvas', 'element.inc.php')); ?>
+                  </div>
+              </div>
 
             <div class="row canvas-row">
                 <div class="column" style="width:100%">
@@ -30,11 +29,7 @@ $canvasName = 'sb';
                 </div>
             </div>
 
-            <div class="row canvas-row">
-                <div class="column" style="width:100%">
-                    <?php $elementName = 'sb_description'; require($this->getTemplatePath('canvas', 'element.inc.php')); ?>
-                </div>
-            </div>
+
 
 			<?php $statusLabels = $stakeholderStatusLabels; ?>
             <div class="row canvas-row" id="stakeholderRow">
