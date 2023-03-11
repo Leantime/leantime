dafa71b04612a942872641a431991a0e01c27507
Marcel Folaron <marcel.folaron@gmail.com>
2023-03-10 22:48:17 -0500

    added missing parameters to mail filter class

--- app/core/class.mailer.php
+++ app/core/class.mailer.php
@@ -227,8 +227,8 @@ namespace leantime\core {
 
             $this->dispatchMailerEvent('beforeSendMail', []);
 
-            $to = $this->dispatchMailerFilter('sendMailTo', $to);
-            $from = $this->dispatchMailerFilter('sendMailFrom', $from);
+            $to = $this->dispatchMailerFilter('sendMailTo', $to, []);
+            $from = $this->dispatchMailerFilter('sendMailFrom', $from, []);
 
             $this->mailAgent->isHTML(true); // Set email format to HTML
 
