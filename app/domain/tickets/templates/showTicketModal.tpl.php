80266d3dc84de388b75d2d48df555d50efde8b95
Marcel Folaron <marcel.folaron@gmail.com>
2023-03-12 13:01:41 -0400

    Ticket View Updates
    - Moving comments onto main screen
    - Toggle for description
    - Comment toggle update

--- app/domain/tickets/templates/showTicketModal.tpl.php
+++ app/domain/tickets/templates/showTicketModal.tpl.php
@@ -25,7 +25,6 @@
     <ul>
         <li><a href="#ticketdetails"><span class="fa fa-star"></span> <?php echo $this->__("tabs.ticketDetails") ?></a></li>
         <li><a href="#subtasks"><span class="fa fa-tasks"></span> <?php echo $this->__('tabs.subtasks') ?> (<?php echo $this->get('numSubTasks'); ?>)</a></li>
-        <li><a href="#comments"><span class="fa fa-comments"></span> <?php echo $this->__("tabs.discussion") ?> (<?php echo $this->get('numComments'); ?>)</a></li>
         <li><a href="#files"><span class="fa fa-file"></span> <?php echo $this->__("tabs.files") ?> (<?php echo $this->get('numFiles'); ?>)</a></li>
         <?php if ($login::userIsAtLeast($roles::$editor)) {  ?>
             <li><a href="#timesheet"><span class="fa fa-clock"></span> <?php echo $this->__("tabs.time_tracking") ?></a></li>
@@ -36,18 +35,10 @@
         <form class="ticketModal" action="<?=BASE_URL ?>/tickets/showTicket/<?php echo $ticket->id ?>" method="post">
             <?php $this->displaySubmodule('tickets-ticketDetails') ?>
         </form>
+
     </div>
 
-    <div id="comments">
-        <form method="post" action="<?=BASE_URL ?>/tickets/showTicket/<?php echo $ticket->id; ?>#comments" class="ticketModal">
-            <input type="hidden" name="comment" value="1" />
-            <?php
-            $this->assign('formUrl', "" . BASE_URL . "/tickets/showTicket/" . $ticket->id . "#comments");
 
-            $this->displaySubmodule('comments-generalComment') ;
-            ?>
-        </form>
-    </div>
 
     <div id="subtasks">
         <?php $this->displaySubmodule('tickets-subTasks') ?>
