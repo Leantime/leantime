e5e3c6b356bd3a7cf2b3f594bcff0d3f3734afdf
Marcel Folaron <marcel.folaron@gmail.com>
2023-09-29 10:34:46 -0400

    fix(menu): Fix handling of empty client names

--- app/Domain/Menu/Templates/partials/projectListFilter.blade.php
+++ app/Domain/Menu/Templates/partials/projectListFilter.blade.php
@@ -10,19 +10,20 @@
                 hx-target="#mainProjectSelector"
                 hx-swap="outerHTML"
                 hx-indicator=".htmx-indicator, .htmx-loaded-content"
-                name="client"
-                id="clientProjectSelect" >
+                name="client">
             <option value="" data-placeholder="true">All Clients</option>
             @foreach ($clients as $client)
-                <option value='{{ $client['id'] }}'
-                @if (isset($projectSelectFilter['clients']) && $projectSelectFilter['clients'] == $client['id'])
-                    selected='selected'
+                @if($client['id'] > 0)
+                    <option value='{{ $client['id'] }}'
+                    @if (isset($projectSelectFilter['clients']) && $projectSelectFilter['clients'] == $client['id'])
+                        selected='selected'
+                    @endif
+                   >{{ $client['name'] }}</option>
                 @endif
-               >{{ $client['name'] }}</option>
             @endforeach
         </select>
         <i class="fa-solid fa-diagram-project"></i>
-        <select data-placeholder="" name="groupBy" id="groupBySelect"
+        <select data-placeholder="" name="groupBy"
                 hx-post="{{ BASE_URL }}/hx/menu/projectSelector/update-menu"
                 hx-target="#mainProjectSelector"
                 hx-indicator=".htmx-indicator, .htmx-loaded-content"
