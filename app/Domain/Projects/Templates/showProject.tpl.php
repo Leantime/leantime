f1721904a82cb1487a105c1aeed4f9ada46ff420
Marcel Folaron <marcel.folaron@gmail.com>
2023-11-30 08:50:54 -0500

    Fix #2105: Handle RTL flag from language files better

--- app/Domain/Projects/Templates/showProject.tpl.php
+++ app/Domain/Projects/Templates/showProject.tpl.php
@@ -536,7 +536,7 @@ $state = $tpl->get('state');
                 currentText: leantime.i18n.__("language.currentText"),
                 closeText: leantime.i18n.__("language.closeText"),
                 buttonText: leantime.i18n.__("language.buttonText"),
-                isRTL: JSON.parse(leantime.i18n.__("language.isRTL")),
+                isRTL: leantime.i18n.__("language.isRTL") === "true" ? 1 : 0,
                 nextText: leantime.i18n.__("language.nextText"),
                 prevText: leantime.i18n.__("language.prevText"),
                 weekHeader: leantime.i18n.__("language.weekHeader"),
