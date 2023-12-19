f1139b967c3bbdf516f4e52675f11d145b7d01a5
Marcel Folaron <marcel.folaron@gmail.com>
2023-12-19 08:40:22 -0500

    update theme colors

--- app/Domain/Auth/Templates/login.tpl.php
+++ app/Domain/Auth/Templates/login.tpl.php
@@ -17,12 +17,14 @@ $redirectUrl = $tpl->get('redirectUrl');
 
 <div class="regcontent">
     <?php $tpl->dispatchTplEvent('afterRegcontentOpen'); ?>
-        <?php if (false === $tpl->get('noLoginForm')) { ?>
-    <form id="login" action="<?=BASE_URL . "/auth/login"?>" method="post">
+    <?php echo $tpl->displayInlineNotification(); ?>
+
+    <?php if (false === $tpl->get('noLoginForm')) { ?>
+        <form id="login" action="<?=BASE_URL . "/auth/login"?>" method="post">
             <?php $tpl->dispatchTplEvent('afterFormOpen'); ?>
         <input type="hidden" name="redirectUrl" value="<?php echo $redirectUrl; ?>" />
 
-            <?php echo $tpl->displayInlineNotification(); ?>
+
 
         <div class="">
             <label for="username">Email</label>
@@ -44,10 +46,10 @@ $redirectUrl = $tpl->get('redirectUrl');
             <?php $tpl->dispatchTplEvent('beforeFormClose'); ?>
 
     </form>
-        <?php } else {
-            echo ($tpl->language->__("text.no_login_form"));
-            ?><br /><br />
-        <?php }// if disableLoginForm ?>
+    <?php } else {
+        echo ($tpl->language->__("text.no_login_form"));
+    ?><br /><br />
+    <?php }// if disableLoginForm ?>
     <?php if ($tpl->get('oidcEnabled')) { ?>
         <?php $tpl->dispatchTplEvent('beforeOidcButton'); ?>
         <div class="">
