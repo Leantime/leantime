5f334c2587222cc51e73f1a0171cf555da6f8ff4
Tanveer Chowdhury <ct.anzoom@gmail.com>
2024-09-04 16:23:59 +0600

    refactor: made changes on text-input based on the feedback

--- app/Views/Templates/components/forms/text-input.blade.php
+++ app/Views/Templates/components/forms/text-input.blade.php
@@ -1,48 +1,54 @@
 @props([
     'inputType' => 'text',
-    'id' => '',
-    'name' => '',
-    'placeholder' => '',
-    'label' => '',
+    'labelText' => '',
+    'labelRight' => '',
     'size' => '',
-    'inputState' => '',
-    'value' => '',
+    'state' => '',
     'leadingVisual' => '',
     'trailingVisual' => '',
+    'caption' => '', 
+    'captionState' => '', 
 ])
 
 @php
     $sizeClass = $size ? 'input-'.$size : '';
-    $stateClass = $inputState ? 'input-'.$inputState : '';
+    $stateClass = $state ? 'input-'.$state : '';
+    $captionClass = $captionState ? 'text-'.$captionState : '';
 @endphp
 
 <div class='par relative w-full max-w-xs'>
-    @if($label)
-        <label for="{{ $id }}">
-            <span class="label-text">{{ $label }}</span>
-        </label>
+    @if($labelText || $labelRight)
+        <div class="flex justify-between">
+            @if($labelText)
+                <label for="{{ $attributes->get('id') }}">
+                    <span class="label-text">{{ $labelText }}</span>
+                </label>
+            @endif
+            @if($labelRight)
+                <span class="label-text-alt">{{ $labelRight }}</span>
+            @endif
+        </div>
     @endif
 
     <div class="relative">
         @if($leadingVisual)
             <span class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <i class="{{ $leadingVisual }}"></i>
+                {{ $leadingVisual }}
             </span>
         @endif
 
         <input 
-            type="{{ $inputType }}" 
-            id="{{ $id }}" 
-            name="{{ $name }}" 
-            placeholder="{{ $placeholder }}" 
-            value="{{ $value }}" 
-            {{$attributes->merge(['class' => 'input input-bordered '.$sizeClass.' '.$stateClass.' '.'w-full max-w-xs '.($leadingVisual ? 'pl-10' : '').($trailingVisual ? 'pr-10' : '')])}}
+            {{$attributes->merge(['class' => 'input input-bordered '.$sizeClass.' '.$stateClass.' w-full max-w-xs '.($leadingVisual ? 'pl-10' : '').($trailingVisual ? 'pr-10' : '')])}}
         />
 
         @if($trailingVisual)
             <span class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                <i class="{{ $trailingVisual }}"></i>
+                {{ $trailingVisual }}
             </span>
         @endif
     </div>
+
+    @if($caption)
+        <span class="label-text-alt {{ $captionClass }}">{{ $caption }}</span>
+    @endif
 </div>
