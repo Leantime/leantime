19622de4c3b32871ad212134434e0f6476829299
DESKTOP-SVBVC1U\HP <muhtasimhafiz@gmail.com>
2024-09-04 21:57:04 +0600

    update the dropdown component

--- app/Domain/Goalcanvas/Templates/dashboard.blade.php
+++ app/Domain/Goalcanvas/Templates/dashboard.blade.php
@@ -57,11 +57,15 @@
                     <x-global::actions.dropdown label-text='All Goal Groups' contentRole="primary" position="bottom">
                         <x-slot:menu>
                             @if ($login::userIsAtLeast($roles::$editor))
-                                <x-global::actions.dropdown.item variant="link" href="#/goalcanvas/bigRock" label-text="{!! __('links.icon.create_new_board') !!}" />
+                                <x-global::actions.dropdown.item variant="link" href="#/goalcanvas/bigRock">
+                                    {!! __('links.icon.create_new_board') !!}
+                                </x-global::actions.dropdown.item>
                                 <x-global::actions.dropdown.item variant="border" />
                             @endif
                             @foreach ($allCanvas as $canvasRow)
-                                <x-global::actions.dropdown.item variant="link" href="{{ BASE_URL }}/goalcanvas/showCanvas/{{ $canvasRow['id'] }}" label-text="{{ $tpl->escape($canvasRow['title']) }}" />
+                                <x-global::actions.dropdown.item variant="link" href="{{ BASE_URL }}/goalcanvas/showCanvas/{{ $canvasRow['id'] }}">
+                                    {{ $tpl->escape($canvasRow['title']) }}
+                                </x-global::actions.dropdown.item>
                             @endforeach
                         </x-slot:menu>
                     </x-global::actions.dropdown>
--- app/Views/Templates/components/actions/dropdown/item.blade.php
+++ app/Views/Templates/components/actions/dropdown/item.blade.php
@@ -1,12 +1,11 @@
 @props([
-    'label-text' => '', // Text or HTML content for the dropdown item
     'variant' => 'link', // Variant of the dropdown item: 'link' or 'border'
 ])
 
 @if ($variant === 'link')
     <li>
         <a {{ $attributes }}>
-            {!! $labelText !!}
+            {!! $slot !!}
         </a>
     </li>
 @elseif ($variant === 'border')
