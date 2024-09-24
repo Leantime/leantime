19727a47b3920cf12acce209de99f05f4a5387eb
Tanveer Chowdhury <ct.anzoom@gmail.com>
2024-09-24 14:40:47 +0600

    added new badge component

--- app/Views/Templates/components/elements/badge.blade.php
+++ app/Views/Templates/components/elements/badge.blade.php
@@ -1,4 +1,31 @@
 @props([
+    'size' => '', //lg, md, sm. xs
+    'type' => '', //neutral, primary, secondary, accent, ghost, info, success, warning, error
+    'outlineState' => false,
+    'leadingVisual' => ''
+])
+
+@php
+    $sizeClass = $size ? 'badge-'.$size : '';
+    $typeClass = $type ? 'badge-'.$type : '';
+    $outlineClass = $outlineState ? 'badge-outline' : '';
+@endphp
+
+
+<div {{ $attributes->merge(['class' => 'badge mt-1 '.$typeClass.' '.$sizeClass.' '.$outlineClass]) }}>
+    @if($leadingVisual)
+        <span class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            {{ $leadingVisual }}>
+        </span>
+    @endif
+    {{ $slot }}
+</div>
+
+
+
+
+
+{{-- @props([
     'asLink' => false,
     'color' => match ($color ?? null) {
         'yellow' => ['yellow-500', 'bg-yellow-500'],
@@ -23,4 +50,4 @@
 </a>
 @else
 </span>
-@endif
+@endif --}}
