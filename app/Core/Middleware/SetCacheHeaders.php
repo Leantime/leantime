66c97e66038db9f2700f03262cac715d0787a95c
Marcel Folaron <marcel.folaron@gmail.com>
2024-11-12 09:41:35 -0500

    Update SetCacheHeaders.php

--- app/Core/Middleware/SetCacheHeaders.php
+++ app/Core/Middleware/SetCacheHeaders.php
@@ -5,7 +5,7 @@ namespace Leantime\Core\Middleware;
 use Closure;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
-use Leantime\Core\Services\AuthService;
+use Leantime\Domain\Auth\Services\Auth as AuthService;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
