7e2ea500500f047460d70747a569b1cd0b756fcd
Marcel Folaron <marcel.folaron@gmail.com>
2024-11-23 08:28:03 -0500

    fix csv importer

--- app/Domain/Connector/Models/Integration.php
+++ app/Domain/Connector/Models/Integration.php
@@ -2,33 +2,51 @@
 
 namespace Leantime\Domain\Connector\Models {
 
+    /**
+     *
+     */
     class Integration
     {
+        #[DbColumn('id')]
         public int $id;
 
+        #[DbColumn('providerId')]
         public ?string $providerId;
 
+        #[DbColumn('method')]
         public ?string $method;
 
+        #[DbColumn('entity')]
         public ?string $entity;
 
+        #[DbColumn('fields')]
         public ?string $fields;
 
+        #[DbColumn('schedule')]
         public ?string $schedule;
 
+        #[DbColumn('notes')]
         public ?string $notes;
 
+        #[DbColumn('auth')]
         public ?string $auth;
 
+        #[DbColumn('meta')]
         public ?string $meta;
 
+        #[DbColumn('createdOn')]
         public ?string $createdOn;
 
+        #[DbColumn('createdBy')]
         public ?string $createdBy;
 
+        #[DbColumn('lastSync')]
         public ?string $lastSync;
 
-        public function __construct() {}
+
+        public function __construct()
+        {
+        }
     }
 
 }
