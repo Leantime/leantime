251fa6e132082f21d8dd29f88d14889349dd2fdb
DESKTOP-SVBVC1U\HP <muhtasimhafiz@gmail.com>
2024-11-27 19:01:36 +0600

    design update

--- app/Domain/Menu/Templates/partials/leftnav/submenu.blade.php
+++ app/Domain/Menu/Templates/partials/leftnav/submenu.blade.php
@@ -1,23 +1,30 @@
 <li class="submenuToggle">
-    <a href="javascript:void(0);"
-       @if ( $menuItem['visual'] !== 'always' )
-           onclick="leantime.menuController.toggleSubmenu('{{ $menuItem['id'] }}')"
-        @endif
-    >
-        <i class="submenuCaret fa fa-angle-{{ $menuItem['visual'] == 'closed' ? 'right' : 'down' }}"
-           id="submenu-icon-{{ $menuItem['id'] }}"></i>
+    <a href="javascript:void(0);" class="flex justify-between items-center"
+        @if ($menuItem['visual'] !== 'always') onclick="leantime.menuController.toggleSubmenu('{{ $menuItem['id'] }}')" @endif>
         <strong>{!! __($menuItem['title']) !!}</strong>
+        <i class="submenuCaret fa fa-angle-{{ $menuItem['visual'] == 'closed' ? 'right' : 'down' }}"
+            id="submenu-icon-{{ $menuItem['id'] }}"></i>
     </a>
-</li>
-<ul id="submenu-{{ $menuItem['id'] }}" class="submenu {{ $menuItem['visual'] == 'closed' ? 'closed' : 'open' }}">
-    @foreach ($menuItem['submenu'] as $subkey => $submenuItem)
-        @switch ($submenuItem['type'])
-            @case('header')
-                @include("menu::partials.leftnav.header", ["menuItem" => $submenuItem, "module" => $module, "action" => $action])
-                @break
-            @case('item')
-                @include("menu::partials.leftnav.item", ["menuItem" => $submenuItem, "module" => $module, "action" => $action])
-        @endswitch
-    @endforeach
-</ul>
 
+    <ul id="submenu-{{ $menuItem['id'] }}" class="ml-4 submenu {{ $menuItem['visual'] == 'closed' ? 'closed' : 'open' }}">
+        @foreach ($menuItem['submenu'] as $subkey => $submenuItem)
+            @switch ($submenuItem['type'])
+                @case('header')
+                    @include('menu::partials.leftnav.header', [
+                        'menuItem' => $submenuItem,
+                        'module' => $module,
+                        'action' => $action,
+                    ])
+                @break
+
+                @case('item')
+                    @include('menu::partials.leftnav.item', [
+                        'menuItem' => $submenuItem,
+                        'module' => $module,
+                        'action' => $action,
+                    ])
+                @break 
+            @endswitch 
+        @endforeach
+    </ul>
+</li>
