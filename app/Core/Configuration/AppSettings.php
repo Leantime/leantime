dba56885921fc3338925a75bfd9e2447885a1c0a
Marcel Folaron <marcel@leantime.io>
2025-03-19 10:07:53 -0400

    task: bump version

--- app/Core/Configuration/AppSettings.php
+++ app/Core/Configuration/AppSettings.php
@@ -7,7 +7,7 @@ namespace Leantime\Core\Configuration;
  */
 class AppSettings
 {
-    public string $appVersion = '3.4.7';
+    public string $appVersion = '3.4.8';
 
     public string $dbVersion = '3.4.0';
 }
