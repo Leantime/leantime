981b103fb8429d370ec313e5c82e0b55f99ab463
Marcel Folaron <marcel@leantime.io>
2025-07-10 12:48:28 -0400

    fix: error when saving idea without title

--- app/Domain/Ideas/Controllers/IdeaDialog.php
+++ app/Domain/Ideas/Controllers/IdeaDialog.php
@@ -214,6 +214,7 @@ class IdeaDialog extends Controller
                     return Frontcontroller::redirect(BASE_URL.'/ideas/ideaDialog/'.(int) $params['itemId']);
                 } else {
                     $this->tpl->setNotification($this->language->__('notification.please_enter_title'), 'error');
+                    return Frontcontroller::redirect(BASE_URL.'/ideas/ideaDialog/');
                 }
             } else {
                 if (isset($_POST['description']) === true) {
@@ -256,6 +257,7 @@ class IdeaDialog extends Controller
                     return Frontcontroller::redirect(BASE_URL.'/ideas/ideaDialog/'.(int) $id);
                 } else {
                     $this->tpl->setNotification($this->language->__('notification.please_enter_title'), 'error');
+                    return Frontcontroller::redirect(BASE_URL.'/ideas/ideaDialog/');
                 }
             }
 
