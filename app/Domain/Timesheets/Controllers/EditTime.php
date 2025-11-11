acc24912bcb5ad2fe4f4d36e9d3cde9ab0408add
Haris Macic <haris.macic@pearshadow.com>
2025-11-11 17:07:03 +0100

    #4794 Make ticket time logs editable

--- app/Domain/Timesheets/Controllers/EditTime.php
+++ app/Domain/Timesheets/Controllers/EditTime.php
@@ -215,7 +215,7 @@ class EditTime extends Controller
                     $this->tpl->assign('allTickets', $this->tickets->getAll());
                     $this->tpl->assign('kind', $this->timesheetsRepo->kind);
 
-                    return $this->tpl->displayPartial('timesheets.editTime');
+                    return $this->tpl->display('timesheets.editTime');
                 } else {
                     return $this->tpl->displayPartial('errors.error403');
                 }
