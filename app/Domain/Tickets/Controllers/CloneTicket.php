cd8c0ac50eb62ec554f831247fb725cc93d4be51
Haris Macic <haris.macic@pearshadow.com>
2025-11-20 11:16:57 +0100

    #4784 Address QA comments, make every tickets detail being cloned

--- app/Domain/Tickets/Controllers/CloneTicket.php
+++ app/Domain/Tickets/Controllers/CloneTicket.php
@@ -18,11 +18,19 @@ class CloneTicket extends Controller
 
     public function post($params): Response
     {
+        error_log(print_r($params, true));
         $originalTicket = $this->ticketService->getTicket($params['id']);
 
         if (!$originalTicket) {
             return Frontcontroller::redirect(BASE_URL.'/tickets/showKanban');
         }
+        $editFrom = new \DateTime($originalTicket->editFrom);
+        $editFrom->modify('+1 hour');
+        $editTo = new \DateTime($originalTicket->editTo);
+        $editTo->modify('+1 hour');
+
+        $dateToFinish = new \DateTime($originalTicket->dateToFinish);
+        $dateToFinish->modify('+1 hour');
 
         $cloneParams = [
             'headline' => $originalTicket->headline,
@@ -32,6 +40,15 @@ class CloneTicket extends Controller
             'editorId' => $originalTicket->editorId,
             'type' => $originalTicket->type,
             'priority' => $originalTicket->priority,
+            'storypoints' => $originalTicket->storypoints,
+            'dateToFinish' => $dateToFinish->format('Y-m-d H:i:s'),
+            'tags' => $originalTicket->tags,
+            'milestoneid' => $originalTicket->milestoneid,
+            'dependingTicketId' => $originalTicket->dependingTicketId,
+            'editFrom' => $editFrom->format('Y-m-d H:i:s'),
+            'editTo' => $editTo->format('Y-m-d H:i:s'),
+            'planHours' => $originalTicket->planHours,
+            'hourRemaining' => $originalTicket->hourRemaining,
         ];
 
         $newTicketId = $this->ticketService->addTicket($cloneParams);
