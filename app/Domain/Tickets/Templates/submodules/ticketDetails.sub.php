0f17e43efbf993b72742cbcae0c4590a2c18eab0
farukvukovic <104840615+farukvukovic@users.noreply.github.com>
2026-01-08 14:51:00 +0100

    #5393 Move comments above ticket history

--- app/Domain/Tickets/Templates/submodules/ticketDetails.sub.php
+++ app/Domain/Tickets/Templates/submodules/ticketDetails.sub.php
@@ -184,6 +184,22 @@ if ($currentUser && isset($currentUser['firstname'], $currentUser['lastname'])) 
             </div>
         </div>
 
+        <?php if ($ticket->id) {?>
+        <h4 class="widgettitle title-light"><span
+                    class="fa-solid fa-comments"></span><?php echo $tpl->__('subtitles.discussion'); ?></h4>
+
+        <div class="row-fluid">
+        <form method="post" action="<?= BASE_URL ?>/tickets/showTicket/<?php echo $ticket->id; ?>" class="formModal">
+            <input type="hidden" name="comment" value="1" />
+            <?php
+            $tpl->assign('formUrl', ''.BASE_URL.'/tickets/showTicket/'.$ticket->id.'');
+
+            $tpl->displaySubmodule('comments-generalComment');
+        ?>
+        </form>
+        </div>
+        <?php } ?>
+
 <div class="status-change-log" id="status-change-log" data-ticket-id="<?= $ticket->id ?>"></div>
 
 <script>
@@ -308,19 +324,6 @@ jQuery(document).ready(function($) {
                 Loading Subtasks ...<br /><br />
             </div>
 
-        <h4 class="widgettitle title-light"><span
-                    class="fa-solid fa-comments"></span><?php echo $tpl->__('subtitles.discussion'); ?></h4>
-
-        <div class="row-fluid">
-        <form method="post" action="<?= BASE_URL ?>/tickets/showTicket/<?php echo $ticket->id; ?>" class="formModal">
-            <input type="hidden" name="comment" value="1" />
-            <?php
-            $tpl->assign('formUrl', ''.BASE_URL.'/tickets/showTicket/'.$ticket->id.'');
-
-            $tpl->displaySubmodule('comments-generalComment');
-        ?>
-        </form>
-        </div>
         <?php } ?>
     </div>
     <div class="col-md-3">
