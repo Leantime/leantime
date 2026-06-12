41eb33fd2a9ce97ea59928ba285d4d05c145ef93
Marcel Folaron <marcel@leantime.io>
2026-06-12 13:23:38 -0400

    release: bump version to 3.9.2 + changelog (#3524)

--- app/Core/Configuration/AppSettings.php
+++ app/Core/Configuration/AppSettings.php
@@ -7,7 +7,7 @@ namespace Leantime\Core\Configuration;
  */
 class AppSettings
 {
-    public string $appVersion = '3.9.1';
+    public string $appVersion = '3.9.2';
 
     public string $dbVersion = '3.5.18';
 }
